e6b9c363dde1ebb780a8257e3d57df07da483c48
Alysson Bormann <alysson.bormann@gmail.com>
2021-09-20 23:35:28 -0400

    criacao de seed admin

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,3 +1,23 @@
-import { createConnection } from "typeorm";
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-createConnection();
+interface IOptions {
+  host: string;
+}
+
+// getConnectionOptions().then((options) => {
+//   const newOptions = options as IOptions;
+//   newOptions.host = "database";
+//   createConnection({
+//     ...options,
+//   });
+// });
+
+export default async (host = "database"): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+
+  return createConnection(
+    Object.assign(defaultOptions, {
+      host,
+    })
+  );
+};
