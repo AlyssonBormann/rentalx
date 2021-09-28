ebea8deeaf13f29c182dc70dfa417ad82dbee70a
Alysson Bormann <alysson.bormann@gmail.com>
2021-09-28 06:43:30 -0400

    createRentals

---
+++ src/shared/infra/typeorm/migrations/1632823964963-CreateRentals.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateRentals1632823964963 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "rentals",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "car_id",
+            type: "uuid",
+          },
+          {
+            name: "user_id",
+            type: "uuid",
+          },
+          {
+            name: "start_date",
+            type: "timestamp",
+          },
+          {
+            name: "end_date",
+            type: "timestamp",
+          },
+          {
+            name: "expect_return_data",
+            type: "timestamp",
+          },
+          {
+            name: "total",
+            type: "numeric",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
