76df758b4fe7b0c5c88efd139d443d6896e47a47
Alysson Bormann <alysson.bormann@gmail.com>
2021-10-17 21:43:36 -0400

    provider de upload

--- src/shared/infra/http/routes/user.routes.ts
+++ src/shared/infra/http/routes/user.routes.ts
@@ -8,7 +8,7 @@ import { UpdateUserController } from "@modules/accounts/UseCases/updateUserAvata
 
 const usersRoutes = Router();
 
-const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserController();
