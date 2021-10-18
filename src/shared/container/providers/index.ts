5082127df06f90adf03ad64e2d55c9ce4c2e3090
Alysson Bormann <alysson.bormann@gmail.com>
2021-10-18 00:02:41 -0400

    commit

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,6 +4,7 @@ import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvi
 import { IMailProvider } from "./MailProvider/IMailProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
 import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
+import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
 import { IStorageProvider } from "./StorageProvider/IStorageProvider";
 
 container.registerSingleton<IDateProvider>(
@@ -15,8 +16,12 @@ container.registerInstance<IMailProvider>(
   "EtherealMailProvider",
   new EtherealMailProvider()
 );
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider,
+};
 
 container.registerSingleton<IStorageProvider>(
   "StorageProvider",
-  LocalStorageProvider
+  diskStorage[process.env.disk]
 );
