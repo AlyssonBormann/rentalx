cd2d8628a806f9de33b7f9da9a25cc78ced6ed7e
Alysson Bormann <alysson.bormann@gmail.com>
2021-10-21 22:32:44 -0400

    indo producao

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,11 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-//host = "database" no meio das ()
+//host = "database" no meio das
 export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
-    Object.assign(defgitaultOptions, {
+    Object.assign(defaultOptions, {
       //host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database:
         process.env.NODE_ENV === "test"
