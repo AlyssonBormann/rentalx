9ff4b912fe2caac9838d814f5337ce82e812f675
Alysson Bormann <alysson.bormann@gmail.com>
2021-10-21 22:32:23 -0400

    md

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -5,7 +5,7 @@ export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
-    Object.assign(defaultOptions, {
+    Object.assign(defgitaultOptions, {
       //host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database:
         process.env.NODE_ENV === "test"
